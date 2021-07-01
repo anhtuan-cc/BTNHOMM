e240578ee1b9cefdf74065c7be79b2ab57c00c31
luongngoc2194 <luongngoc2194@gmail.com>
2021-07-01 19:37:52 +0700

    DogPage-Home

---
+++ js/headerAndFooter.js
@@ -0,0 +1,71 @@
+function header() {
+    var header_txt = "<div class=\"container\">\n" +
+        "            <div class=\"row \">\n" +
+        "                <div class=\"tab col-md-12\">\n" +
+        "\n" +
+        "                    <div class=\"row\">\n" +
+        "                        <div class=\"col-md-8 left\">\n" +
+        "                            <h1><a href=\"#\">Pet's Family</a></h1>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"col-md-4 right\">\n" +
+        "                            <ul>\n" +
+        "                                <li><a href=\"#\">Giới Thiệu</a></li>\n" +
+        "                                <li><a href=\"#\">Kinh Nghiệm</a></li>\n" +
+        "                                <li><a href=\"tintuc.html\">Tin Tức</a></li>\n" +
+        "                                <li><a href=\"\">Trang Bị</a></li>\n" +
+        "                            </ul>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "\n" +
+        "            <div class=\"row\">\n" +
+        "                <div class=\"status col-md-12\">\n" +
+        "                    <h1>LOVE YOUR DOGS</h1>\n" +
+        "                    <h3>When pets are family, the benefits extend into society</h3>\n" +
+        "                    <div class=\"col-md-4\"></div>\n" +
+        "                    <div class=\"col-md-4\">\n" +
+        "                        <hr style=\"border:1px solid blue;\">\n" +
+        "                    </div>\n" +
+        "                    <div class=\"col-md-4\"></div>\n" +
+        "\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>"
+
+    document.getElementById("header").innerHTML = header_txt;
+}
+header();
+function foodter() {
+    var fooder_txt = "<div class=\"container\">\n" +
+        "            <div class=\"row\">\n" +
+        "                <div class=\"col-md-4\">\n" +
+        "                    <h3>Dog's Familly</h3>\n" +
+        "                    <ul>\n" +
+        "                        <li><a href=\"#\">Giới Thiệu</a></li>\n" +
+        "                        <li><a href=\"#\">Kinh Nghiệm</a></li>\n" +
+        "                        <li><a href=\"#\">Tin Tức</a></li>\n" +
+        "                        <li><a href=\"\">Trang Bị</a></li>\n" +
+        "                    </ul>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-4\">\n" +
+        "                    <h3>Introduce</h3>\n" +
+        "                    <p>We are a non-profit organization. We do not accept or solicit sponsorship. Our video aims to raise public awareness about the protection of animals, dogs and cats</p>\n" +
+        "\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-4\">\n" +
+        "                    <h3>Contact Us</h3>\n" +
+        "                    <p>Call us : (+84) 24-7300-8855, (+84) 24-7300-8855 </p>\n" +
+        "                    <p>Email us : <a href=\"#\">demo@mail.com</a>, <a href=\"#\">fpt-aptech@mail.com</a></p>\n" +
+        "                    <p>Walk in : Số 8, Tôn Thất Thuyết, Mỹ Đình, Từ Liêm, Hà Nội <br>590 Cách Mạng Tháng 8, Quận 3, TPHCM</p>\n" +
+        "\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>"
+
+    document.getElementById("foodter").innerHTML = fooder_txt;
+}
+foodter();
+
+
+
